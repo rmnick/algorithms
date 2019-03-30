f8d87519b4e9e648f6f834e11be270b3236a8bf2
rmnick <r.m.nick@yandex.ru>
2019-03-31 00:28:04 +0500

    select correct

--- algorithms/sorters/src/main/java/ru/chubaka/select/SelectSort.java
+++ algorithms/sorters/src/main/java/ru/chubaka/select/SelectSort.java
@@ -11,16 +11,18 @@ public class SelectSort {
 		Service.showArr(oddArr);
 		Service.showArr(evenArr);
 		oddArr = sorter.sort(oddArr);
+                evenArr = sorter.sort(evenArr);
 		Service.showArr(oddArr);
+                Service.showArr(evenArr);
 	}
 
 	public int[] sort(final int[] arr) {
 		int min;
 		int temp;
 		int next;
-		for (int i = 0; i < arr.lenght - 1; i++) {
+		for (int i = 0; i < arr.length - 1; i++) {
 			min = i;
-			for (next = i + 1; next < arr.lenght; next++) {
+			for (next = i + 1; next < arr.length; next++) {
 				if (arr[next] < arr[min]) {
 					min = next;
 				}
